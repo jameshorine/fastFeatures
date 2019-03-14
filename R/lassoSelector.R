8e5e46a6d1f7ac0ec4c7b4fc0715fa49107616c3
jameshorine <jamespatrickhorine@gmail.com>
2019-03-14 00:55:05 -0500

    adding lassoSellector

---
+++ R/lassoSelector.R
@@ -0,0 +1,38 @@
+#'
+#'
+#'
+#'
+
+#' @export
+lassoSelector <- function(df, target_column, feature_columns, column_proportion, n_iterations, l1_lambda, glmnet_family){
+
+  num_cores <- parallel::detectCores()
+
+  set.seed(seed = 1234, kind = "Mersenne-Twister")
+
+  temp_results <- pbmcapply::pbmclapply(X = 1:n_iterations,
+                                        FUN = function(X){
+                                          random_columns <- sample(x = feature_variables,
+                                                                   size = round(x = column_proportion*length(feature_variables),
+                                                                                digits = 0),
+                                                                   replace = FALSE)
+
+                                          temp_mdl <- glmnet::glmnet(x = data.matrix(df[,random_columns, with = F]),
+                                                                     y = data.matrix(df[,target_variable, with = F]),
+                                                                     family = glmnet_family,
+                                                                     alpha = 1, lambda = l1_lambda,
+                                                                     intercept = FALSE)
+
+                                          temp_coefs_table <- data.table::data.table(as.data.frame(as.matrix(coef(temp_mdl))), keep.rownames = TRUE)
+                                          return(temp_coefs_table)
+                                        },
+                                        mc.cores = num_cores
+  )
+
+  res <- data.table:::merge.data.table(x = data.table::rbindlist(temp_results)[rn != "(Intercept)"][, list(count = .N), by = rn],
+                                       y = data.table::rbindlist(temp_results)[rn != "(Intercept)"][s0 > 0, list(countInc = .N), by = rn],
+                                       by = "rn")[ , list("Variable" = rn, "Conditional Variable Inclusion Probability" = countInc / count)]
+
+  return(res)
+}
+
