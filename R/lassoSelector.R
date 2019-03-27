dbe068a1e5556427f292802916a9ea8ee733610d
jameshorine <jamespatrickhorine@gmail.com>
2019-03-27 13:19:57 -0500

    adding function options and checks

--- R/lassoSelector.R
+++ R/lassoSelector.R
@@ -5,7 +5,8 @@
 #' @param df \code{data.table}-type data matrix
 #' @param target_column Character string identifying the target column
 #' @param feature_columns Character vector identifying the feature columns to investigate
-#' @param column_proportion Numeric on the interval (0,1), this is the percentage of columns randomly sampeled
+#' @param column_proportion Numeric on the interval (0,1), this is the percentage of columns randomly sampeled without replacement.
+#' @param record_proportion Numeric on the interval (0,1], default is 1. This is the percentage of records to random select for boot strap sampling, with replacement.
 #' @param n_iterations The number of bootstrap replications
 #' @param l1_lambda The LASSO L1 penalty parameter
 #' @param glmnet_family Character string, see glmnet for more information
@@ -24,11 +25,33 @@
 #' \item Knockoff Filtering as presented in Data Science and Predictive Analytics (UMich HS650). http://www.socr.umich.edu/people/dinov/courses/DSPA_notes/17_RegularizedLinModel_KnockoffFilter.html#10_knockoff_filtering
 #' }
 #'
-#' @details \code{lassoSelector} is a parallelized Bootstrap LASSO built on the \code{glmnet} package from Hastie et. al. The underlying glmnet framework does not appear to be compatiable with \code{data.table}, as such, \code{data.table}-
+#' @details \code{lassoSelector} is a parallelized Bootstrap LASSO built on the \code{glmnet} package from Hastie et. al. The underlying glmnet framework does not appear to be compatiable with \code{data.table}, as such, \code{data.table}-type inputs will be down-cast to \code{base::data.matrix}-types when needed.
 #'
 #' @export
 
-lassoSelector <- function(df, target_column, feature_columns, column_proportion, n_iterations, l1_lambda, glmnet_family){
+lassoSelector <- function(df, target_column, feature_columns, column_proportion, record_proportion = 1, n_iterations, l1_lambda, glmnet_family){
+  ####
+  defined <- setdiff(ls(), "record_proportion")
+  passed <- names(as.list(match.call())[-1])
+  if(any(!defined %in% passed)) {
+    stop(paste("Please supply function values for:",paste(setdiff(defined, passed),collapse=", ")))
+  }
+
+  if(data.table::is.data.table(df)) { }
+  else {stop('Input df must be a data.table::data.table() type object.')}
+
+  if(is.character(target_variable)) { }
+  else {stop('Input target_variable must be a base::character() type object.')}
+
+  if(is.character(feature_variables)) { }
+  else {stop('Input feature_variables must be a base::character() type object.')}
+
+  if(length(feature_variables) > 1) { }
+  else {stop('Input feature_variables should be a long acharacter vector type object. This check only tests "length > 1".')}
+
+  if(record_proportion <= 1 && record_proportion > 0) { }
+  else {stop('Input record_proportion must be a number on the  interval (0,1]. Default is 1 (no record bootstrapping).')}
+  ####
 
   num_cores <- parallel::detectCores()
 
@@ -41,12 +64,24 @@ lassoSelector <- function(df, target_column, feature_columns, column_proportion,
                                                                                 digits = 0),
                                                                    replace = FALSE)
 
-                                          temp_mdl <- glmnet::glmnet(x = data.matrix(df[,random_columns, with = F]),
-                                                                     y = data.matrix(df[,target_variable, with = F]),
+                                          if(record_proportion == 1){
+                                            temp_mdl <- glmnet::glmnet(x = data.matrix(df[,random_columns, with = F]),
+                                                                       y = data.matrix(df[,target_variable, with = F]),
+                                                                       family = glmnet_family,
+                                                                       alpha = 1, lambda = l1_lambda,
+                                                                       intercept = FALSE)
+                                          }
+                                          else {random_rows <- sample(x = 1:dim(df)[1],
+                                                                      size = round(x = dim(df)[1]*record_proportion,
+                                                                                   digits = 0),
+                                                                      replace = TRUE)
+
+                                          temp_mdl <- glmnet::glmnet(x = data.matrix(df[random_rows,random_columns, with = F]),
+                                                                     y = data.matrix(df[random_rows,target_variable, with = F]),
                                                                      family = glmnet_family,
                                                                      alpha = 1, lambda = l1_lambda,
                                                                      intercept = FALSE)
-
+                                          }
                                           return(data.table::data.table(as.data.frame(as.matrix(coef(temp_mdl))), keep.rownames = TRUE))
                                         },
                                         mc.cores = num_cores
@@ -58,4 +93,3 @@ lassoSelector <- function(df, target_column, feature_columns, column_proportion,
 
   return(res)
 }
-
--- README.md
+++ README.md
@@ -1,47 +1,69 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# fastFeatures
+# `fastFeatures`
 
-The goal of fastFeatures is to …
+`fastFeatures` is built from the ground up with one goal in mind:
+Quickly slice and dice through feature spaces too large to quickly
+manually examine so that you can build a predictive model that is easier
+to explain to explain to non-technical stakeholders. `fastFeatures`
+provides an easy-to-use interface to fast variable selection methods.
+The implemented feature selection functions are:
+
+1.  `lassoSelector`. lassoSelector is built from the notion of
+    Conditional Variable Inclusion Probability as introduced by Bunea
+    et. al \[1\].
+
+2.  comming soon
 
 ## Installation
 
 You can install the released version of fastFeatures from
-[CRAN](https://CRAN.R-project.org) with:
+[GitHub](https://github.com/) with:
+
+1.  
+<!-- end list -->
 
 ``` r
-install.packages("fastFeatures")
+install.packages("devtools")
 ```
 
-## Example
-
-This is a basic example which shows you how to solve a common problem:
+2.  
+<!-- end list -->
 
 ``` r
-## basic example code
+devtools::install_github("jameshorine/fastFeatures")
 ```
 
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
+## Useage
 
-``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
-```
+The general recipie for using this package is:
 
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date.
+1.  Prepare data appropriately.
+2.  Assign your data, target, and feature variables to convinent objects
+      - data, target, features, are some easy to remember choices
+3.  Run your prefered method.
+4.  Interpret results.
 
-You can also embed plots, for example:
+## Refernences
 
-<img src="man/figures/README-pressure-1.png" width="100%" />
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub\!
+1.  Bunea, Florentina et al. “Penalized least squares regression methods
+    and applications to neuroimaging” NeuroImage vol. 55,4 (2010):
+    1519-27.
+2.  Abram, Samantha V et al. “Bootstrap Enhanced Penalized Regression
+    for Variable Selection with Neuroimaging Data” Frontiers in
+    neuroscience vol. 10 344. 28 Jul. 2016,
+    <doi:10.3389/fnins.2016.00344>
+3.  Santosa, Fadil; Symes, William W. (1986). “Linear inversion of
+    band-limited reflection seismograms”. SIAM Journal on Scientific and
+    Statistical Computing. SIAM. 7 (4): 1307–1330. <doi:10.1137/0907087>
+4.  Tibshirani, Robert (1996). “Regression Shrinkage and Selection via
+    the lasso”. Journal of the Royal Statistical Society. Series B
+    (methodological). Wiley. 58 (1): 267–88. JSTOR 2346178
+5.  Jerome Friedman, Trevor Hastie, Robert Tibshirani (2010).
+    Regularization Paths for Generalized Linear Models via Coordinate
+    Descent. Journal of Statistical Software, 33(1), 1-22. URL
+    <http://www.jstatsoft.org/v33/i01/>.
+6.  Knockoff Filtering as presented in Data Science and Predictive
+    Analytics (UMich HS650).
+    <http://www.socr.umich.edu/people/dinov/courses/DSPA_notes/17_RegularizedLinModel_KnockoffFilter.html#10_knockoff_filtering>
