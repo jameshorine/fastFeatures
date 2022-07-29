b5b0c9704e086e8f9fe437d2159b8714cc9207db
csprock <csprock@gmail.com>
2022-07-28 18:55:25 -0600

    added internal test data. added basic unit test. modified DESCRIPTION. removed Rstudio project file

---
+++ tests/testthat/test-cVIP.R
@@ -0,0 +1,24 @@
+test_that("main function works", {
+
+  test_data <- as.data.table(fastFeatures:::test_data_1)
+  print(test_data)
+
+  results <- fastFeatures::cVIP(
+    df = test_data,
+    target_column = "y",
+    feature_columns = c("x1", "x2", "x3"),
+    column_proportion=0.75,
+    record_proportion=0.99,
+    n_iterations=10,
+    l1_lambda=3,
+    glmnet_family="gaussian"
+  )
+
+  expect_s3_class(results, "data.table")
+
+})
+
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
