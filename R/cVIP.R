8a5fa3223f99269dd19198eaac6688c11ba1fdc8
csprock <csprock@gmail.com>
2022-07-30 09:58:29 -0600

    removed unnecessary checking of required variable. moved input type checking into a separate function

--- R/cVIP.R
+++ R/cVIP.R
@@ -33,30 +33,10 @@
 #' @export
 
 cVIP <- function(df, target_column, feature_columns, column_proportion, record_proportion = 0.05, n_iterations, l1_lambda, glmnet_family){
-  ####
-  defined <- setdiff(ls(), "record_proportion")
-  passed <- names(as.list(match.call())[-1])
-  if(any(!defined %in% passed)) {
-    stop(paste("Please supply function values for:",paste(setdiff(defined, passed),collapse=", ")))
-  }
 
-  if(data.table::is.data.table(df)) { }
-  else {stop('Input df must be a data.table::data.table() type object.')}
-
-  if(is.character(target_column)) { }
-  else {stop('Input target_column must be a base::character() type object.')}
-
-  if(is.character(feature_columns)) { }
-  else {stop('Input feature_columns must be a base::character() type object.')}
-
-  if(length(feature_columns) > 1) { }
-  else {stop('Input feature_columns should be a character vector-type object. This check only tests "length > 1".')}
-
-  ####
+  validate_user_input(df, target_column ,feature_columns)
 
   num_cores <- parallel::detectCores()
-
-  set.seed(seed = 1234, kind = "Mersenne-Twister")
 
   MN <- dim(df)
 
