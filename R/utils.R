a7b01e1aea0a3de9043066381de23c963f6d80b5
csprock <csprock@gmail.com>
2022-07-30 09:28:31 -0600

    added separate function for sampling rows and columns and a test

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,20 @@ sample_indices <- function(feature_columns, nrows_, column_proportion, record_pr
 
     return(list(random_rows=random_rows, random_columns=random_columns))
 }
+
+
+sample_matrix <- function(df, target_column, feature_columns, column_proportion, record_proportion) {
+
+    sample_idx <- sample_indices(
+        feature_columns=feature_columns,
+        nrows_=dim(df)[1],
+        record_proportion=record_proportion,
+        column_proportion=column_proportion
+    )
+
+    output <- list(
+        x=data.matrix(df[sample_idx$random_rows, sample_idx$random_columns, with=FALSE]),
+        y=data.matrix(df[sample_idx$random_rows, target_column, with=FALSE])
+    )
+    return(output)
+}
